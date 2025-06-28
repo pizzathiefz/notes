acfaa4b6ab4f5cd75d99357334847cd142d412bd
youngshin <pizzathief0@gmail.com>
2025-06-28 16:49:38 +0900

    remove explorer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+
   ],
   right: [
     Component.Graph(),
@@ -62,7 +62,6 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
   ],
   right: [],
 }
